3237cf00bee74690d6767cb1d699f8adf08a2822
Hamdan KT <hamdankt@onlinesafe.in>
2025-09-16 13:10:23 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,378 @@
+export const myProjects = [
+	{
+		id: 1,
+		title: "Multi-Module Banking Dashboard - Canara Bank",
+		description:
+			"A comprehensive multi-module dashboard for Canara Bank, integrating critical banking datasets to enhance transparency and operational efficiency.",
+		subDescription: [
+			"Created a comprehensive multi-module dashboard integrating critical banking datasets, including SARFAESI, NPA, NF, service matters, legal case data, and staff-related information.",
+			"Designed the dashboard as a single source of truth for management and operational teams, ensuring accessibility of consolidated insights in one place.",
+			"Enhanced case and compliance tracking with dedicated modules for SARFAESI and legal matters, ensuring regulatory adherence.",
+			"Ensured the dashboard delivered scalability, accuracy, and user-friendly visualizations to cater to both technical and non-technical stakeholders.",
+			"Strengthened transparency and operational efficiency, empowering senior management with actionable insights.",
+		],
+		href: "",
+		logo: "",
+		image: "/assets/projects/accessories.jpg",
+		tags: [
+			{
+				id: 1,
+				name: "React",
+				path: "/assets/logos/react.svg",
+			},
+			{
+				id: 2,
+				name: "Redux",
+				path: "/assets/logos/redux.svg",
+			},
+			{
+				id: 3,
+				name: "NodeJS",
+				path: "/assets/logos/node.svg",
+			},
+			{
+				id: 4,
+				name: "TailwindCSS",
+				path: "/assets/logos/tailwindcss.svg",
+			},
+			{
+				id: 5,
+				name: "Ms SQL",
+				path: "/assets/logos/microsoft.svg",
+			},
+			{
+				id: 6,
+				name: "Express",
+				path: "/assets/logos/express.svg",
+				bgColor: "#ffffff",
+			},
+		],
+	},
+	{
+		id: 2,
+		title: "KSSFCL (Karnataka State Souharda Federal Cooperative LTD)",
+		description:
+			"An Overall Management System for KSSFCL, a cooperative society in Karnataka, India, with multiple modules to handle various operations.",
+		subDescription: [
+			"Court Module: Manages court-related data, e-case filing, approval processes, and petition filings.",
+			"Training Module: Handles trainee registrations, exam details, class schedules, and related information.",
+			"Magazine Module: Oversees printing operations, book and author details, and subscription management.",
+			"Secretariat Module: Facilitates election processes and manages election results.",
+			"Admin Module: Centralized management of all other modules.",
+		],
+		href: "",
+		logo: "",
+		image: "/assets/projects/auth-system.jpg",
+		tags: [
+			{
+				id: 1,
+				name: "React",
+				path: "/assets/logos/react.svg",
+			},
+			{
+				id: 2,
+				name: "GraphQL",
+				path: "/assets/logos/graphql.svg",
+			},
+			{
+				id: 3,
+				name: "NodeJS",
+				path: "/assets/logos/node.svg",
+			},
+			{
+				id: 9,
+				name: "Express",
+				path: "/assets/logos/express.svg",
+			},
+			{
+				id: 4,
+				name: "Azure",
+				path: "/assets/logos/azure.svg",
+			},
+			{
+				id: 5,
+				name: "Ms SQL",
+				path: "/assets/logos/microsoft.svg",
+			},
+			{
+				id: 6,
+				name: "Sass",
+				path: "/assets/logos/sass.svg",
+			},
+			{
+				id: 7,
+				name: "MUI",
+				path: "/assets/logos/mui.svg",
+			},
+			{
+				id: 8,
+				name: "Redux",
+				path: "/assets/logos/redux.svg",
+			},
+		],
+	},
+	{
+		id: 3,
+		title: "SMA (Karnataka Vikas Grameena Bank)",
+		description:
+			"An internal application for Karnataka Vikas Grameena Bank (KVGB) to manage various banking operations, including customer management, loan processing, and document handling.",
+		subDescription: [
+			"mplemented a secure and efficient three-tier login system, enhancing user authentication and access control.",
+			"Improved accuracy and reduced documentation time through automation.",
+			"Ensured timely communication and task tracking. Enabled customers to explore and access various bank facilities.",
+			"Enhanced the efficiency of loan processing and oversight, ensuring compliance and improving turnaround times for Karnataka Vikas Grameena Bank (KVGB)",
+		],
+		href: "",
+		logo: "",
+		image: "/assets/projects/blazor-app.jpg",
+		tags: [
+			{
+				id: 1,
+				name: "React",
+				path: "/assets/logos/react.svg",
+			},
+			{
+				id: 3,
+				name: "NodeJS",
+				path: "/assets/logos/node.svg",
+			},
+			{
+				id: 9,
+				name: "Express",
+				path: "/assets/logos/express.svg",
+			},
+			{
+				id: 4,
+				name: "Azure",
+				path: "/assets/logos/azure.svg",
+			},
+			{
+				id: 5,
+				name: "Ms SQL",
+				path: "/assets/logos/microsoft.svg",
+			},
+			{
+				id: 6,
+				name: "Sass",
+				path: "/assets/logos/sass.svg",
+			},
+			{
+				id: 7,
+				name: "MUI",
+				path: "/assets/logos/mui.svg",
+			},
+			{
+				id: 8,
+				name: "Redux",
+				path: "/assets/logos/redux.svg",
+			},
+		],
+	},
+	{
+		id: 4,
+		title: "EOFFICE (ARTCO Ltd.)",
+		description:
+			"A workflow management system for cooperative banks to streamline and automate the approval process for various banking operations.",
+		subDescription: [
+			"Developed a system to automate and streamline the approval process for cooperative banks.",
+			"Implemented workflows for key roles including President, Secretary, Manager, Section Officer, Clerk, and Director.",
+			"Reduced approval times and increased operational efficiency through automated processes.",
+			"Ensured adherence to compliance standards and improved documentation accuracy.",
+		],
+		href: "",
+		logo: "",
+		image: "/assets/projects/game-engine.jpg",
+		tags: [
+			{
+				id: 1,
+				name: "React",
+				path: "/assets/logos/react.svg",
+			},
+			{
+				id: 3,
+				name: "NodeJS",
+				path: "/assets/logos/node.svg",
+			},
+			{
+				id: 9,
+				name: "Express",
+				path: "/assets/logos/express.svg",
+			},
+			{
+				id: 4,
+				name: "Azure",
+				path: "/assets/logos/azure.svg",
+			},
+			{
+				id: 5,
+				name: "Ms SQL",
+				path: "/assets/logos/microsoft.svg",
+			},
+			{
+				id: 6,
+				name: "Sass",
+				path: "/assets/logos/sass.svg",
+			},
+			{
+				id: 7,
+				name: "MUI",
+				path: "/assets/logos/mui.svg",
+			},
+			{
+				id: 8,
+				name: "Redux",
+				path: "/assets/logos/redux.svg",
+			},
+		],
+	},
+	{
+		id: 5,
+		title: "GASEASE",
+		description:
+			"A web application designed to manage gas agency operations, including branch management, safety inspections, user account creation, and SMS notifications for customers.",
+		subDescription: [
+			"A comprehensive solution designed to streamline operations for gas agencies and enhance customer convenience.",
+			"Easily assign and reassign branches to manage service areas effectively.",
+			"Schedule and manage safety inspections for gas installations.",
+			"Simplify the process of creating new user accounts for customers and staff.",
+			"Keep customers informed with automated SMS notifications for bookings, deliveries, and updates.",
+		],
+		href: "",
+		logo: "",
+		image: "/assets/projects/wordpress-theme.jpg",
+		tags: [
+			{
+				id: 1,
+				name: "React",
+				path: "/assets/logos/react.svg",
+			},
+			{
+				id: 3,
+				name: "NodeJS",
+				path: "/assets/logos/node.svg",
+			},
+			{
+				id: 9,
+				name: "Express",
+				path: "/assets/logos/express.svg",
+			},
+			{
+				id: 4,
+				name: "Azure",
+				path: "/assets/logos/azure.svg",
+			},
+			{
+				id: 5,
+				name: "Ms SQL",
+				path: "/assets/logos/microsoft.svg",
+			},
+			{
+				id: 6,
+				name: "Sass",
+				path: "/assets/logos/sass.svg",
+			},
+			{
+				id: 7,
+				name: "MUI",
+				path: "/assets/logos/mui.svg",
+			},
+			{
+				id: 8,
+				name: "Redux",
+				path: "/assets/logos/redux.svg",
+			},
+		],
+	},
+];
+
+export const mySocials = [
+	{
+		name: "WhatsApp",
+		href: "",
+		icon: "/assets/socials/whatsApp.svg",
+	},
+	{
+		name: "Linkedin",
+		href: "https://www.linkedin.com/in/ali-sanati/",
+		icon: "/assets/socials/linkedIn.svg",
+	},
+	{
+		name: "Instagram",
+		href: "https://www.instagram.com/ali.sanatidev/reels/",
+		icon: "/assets/socials/instagram.svg",
+	},
+];
+
+export const experiences = [
+	{
+		title: "Software Developer Intern (MERN)",
+		job: "Soften Technologies",
+		date: "07/2022-10/2022",
+		contents: [
+			"During my internship at Softern, I independently learned various programming languages",
+			"and technologies, assessed new students as an interviewer, and provided feedback,",
+			"showcasing my adaptability, communication skills, and ability to evaluate effectively.",
+		],
+	},
+	{
+		title: "Software Developer (MERN)",
+		job: "Safe Software and Integrated Solutions (Fintech)",
+		date: "2022-Present",
+		contents: [
+			"Skilled React.js and Node.js Developer working with teams to ideate and implement",
+			"software solutions in the fintech sector. Proficient in designing architectures, managing",
+			"MongoDB and SQL databases, and developing APls with Node.js. Dedicated to ensuring",
+			"software efficiency through testing and troubleshooting.",
+		],
+	},
+];
+export const reviews = [
+	{
+		name: "Jack",
+		username: "@jack",
+		body: "I've never seen anything like this before. It's amazing. I love it.",
+		img: "https://robohash.org/jack",
+	},
+	{
+		name: "Jill",
+		username: "@jill",
+		body: "I don't know what to say. I'm speechless. This is amazing.",
+		img: "https://robohash.org/jill",
+	},
+	{
+		name: "John",
+		username: "@john",
+		body: "I'm at a loss for words. This is amazing. I love it.",
+		img: "https://robohash.org/john",
+	},
+	{
+		name: "Alice",
+		username: "@alice",
+		body: "This is hands down the best thing I've experienced. Highly recommend!",
+		img: "https://robohash.org/alice",
+	},
+	{
+		name: "Bob",
+		username: "@bob",
+		body: "Incredible work! The attention to detail is phenomenal.",
+		img: "https://robohash.org/bob",
+	},
+	{
+		name: "Charlie",
+		username: "@charlie",
+		body: "This exceeded all my expectations. Absolutely stunning!",
+		img: "https://robohash.org/charlie",
+	},
+	{
+		name: "Dave",
+		username: "@dave",
+		body: "Simply breathtaking. The best decision I've made in a while.",
+		img: "https://robohash.org/dave",
+	},
+	{
+		name: "Eve",
+		username: "@eve",
+		body: "So glad I found this. It has changed the game for me.",
+		img: "https://robohash.org/eve",
+	},
+];
